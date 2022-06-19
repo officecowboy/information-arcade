ad382032343f43f9f5631ab0f703ca37f1832855
Scott Diekema <scott.diekema@gmail.com>
2022-06-19 19:01:31 -0400

    add cracked wisdom!

--- cracked-wisdom/generator.js
+++ cracked-wisdom/generator.js
@@ -1,14 +1,14 @@
 function generator() {
 
   const btn = document.querySelector("#button")
-  let picUrl = "https://api.catboys.com/img"
+  let picUrl = "https://cracked-wisdom.herokuapp.com/phones/random"
   let textUrl = "https://cracked-wisdom.herokuapp.com/quotes/random"
 
   fetch(picUrl)
     .then(res => res.json())
     .then(data => {
       let image = document.querySelector(".pics");
-      image.src = data.url;
+      image.src = data[0].url;
     })
 
   fetch(textUrl)
@@ -17,7 +17,7 @@ function generator() {
       let quote = document.querySelector(".quote");
       quote.innerHTML = data[0].quote;
       let author = document.querySelector(".author");
-      author.innerHTML = "–" + data[0].author;
+      author.innerHTML = "– " + data[0].author;
     })
 
   btn.addEventListener("click", function () {
@@ -26,7 +26,7 @@ function generator() {
       .then(res => res.json())
       .then(data => {
         let image = document.querySelector(".pics");
-        image.src = data.url
+        image.src = data[0].url;
       })
 
     fetch(textUrl)
