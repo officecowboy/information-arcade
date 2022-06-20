057496cdef642911fc1aef5af1228fa8328fadcd
Scott Diekema <scott.diekema@gmail.com>
2022-06-19 23:46:19 -0400

    test html2canvas download

---
+++ cracked-wisdom/download.js
@@ -0,0 +1,33 @@
+
+setUpDownloadPageAsImage();
+
+function setUpDownloadPageAsImage() {
+  document.getElementById("download-image").addEventListener("click", function () {
+    html2canvas(document.querySelector(".canvas"), { allowTaint: true }).then(function (canvas) {
+      console.log(canvas);
+      simulateDownloadImageClick(canvas.toDataURL(), 'file-name.png');
+    });
+  });
+}
+
+function simulateDownloadImageClick(uri, filename) {
+  var link = document.createElement('a');
+  if (typeof link.download !== 'string') {
+    window.open(uri);
+  } else {
+    link.href = uri;
+    link.download = filename;
+    accountForFirefox(clickLink, link);
+  }
+}
+
+function clickLink(link) {
+  link.click();
+}
+
+function accountForFirefox(click) { // wrapper function
+  let link = arguments[1];
+  document.body.appendChild(link);
+  click(link);
+  document.body.removeChild(link);
+}
